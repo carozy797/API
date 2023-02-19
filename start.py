7708d38c3be6e6d436ae6d0fcaba22b214941fef
daniel <adanielagyei@gmail.com>
2023-02-19 21:39:08 +0000

    getting item at key

--- start.py
+++ start.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+inverntory = {
+    1: {
+    "name": "milk",
+    "price": "10.00",
+    "brand": "Ideal",
 
-@app.get("/")
-def home():
-    return {"Data": "Test"}
+    }
+}
+# creating end point
+@app.get("/get-items/{itemId}")
+def get_items(itemId: int):
+    return inverntory[itemId]
+
+# @app.get("/")
+# def home():
+#     return {"Data": "Testing"}
+ 
