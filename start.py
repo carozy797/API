58657e98308be455fa7659920ab4402175edbeaa
daniel <adanielagyei@gmail.com>
2023-02-25 11:45:46 +0000

    query in api

--- start.py
+++ start.py
@@ -1,15 +1,24 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Query
+from typing import Optional
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    price: float
+    brand: Optional[str] = None
+
 
 app = FastAPI()
-inverntory = {
-    1: {
-    "name": "milk",
-    "price": "10.00",
-    "brand": "Ideal",
-
-    }
-}
+inventory = {}
 # creating end point with path parameter
 @app.get("/get-items/{itemId}")
 def get_items(itemId: int = Path(None, description = "the id of the item")):
-    return inverntory.get(itemId)
+    return inventory.get(itemId)
+# getting item by name using query
+@app.get("/get-items-by-name")
+def get_items_by_name(name : str = Query(None, description = "the name of the item", title = "Name")):
+    for item_id in inventory:
+        if inventory[item_id].name == name:
+            return inventory[item_id]
+    return {"Data": "Not found"}
+
